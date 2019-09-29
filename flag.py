9f0526f12cb791b2898c102c91a141cc79a4dc6e
Исаева Мария <isaevam2015@gmail.com>
2019-09-30 00:24:57 +0400

    слили ветки в одну

--- flag.py
+++ flag.py
@@ -1,6 +1,17 @@
-if __name__ == "__main__":
-    print("drawFlag")
-    print("hgfgjh")
-    print("hh")
-    print("trtrt")
-    print('all')
+import pygame
+
+pygame.init()
+size = width, heigth = 300, 300
+screen = pygame.display.set_mode(size)
+
+def draw():
+    rect_color = pygame.Color('red')
+    rect_width = 0
+    rect_point = [[10,10], [280, 280]]
+    pygame.draw.rect(screen, rect_color, rect_point, rect_width)
+
+draw()
+while pygame.event.wait().type != pygame.QUIT:
+    pygame.display.flip()
+
+pygame.quit()
