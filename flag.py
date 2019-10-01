975f5f84bdd65fd5d072f19d0e8d9e124d74feac
Исаева Мария <isaevam2015@gmail.com>
2019-10-01 23:24:42 +0400

    нарисован флагшток

--- flag.py
+++ flag.py
@@ -5,9 +5,9 @@ size = width, heigth = 300, 300
 screen = pygame.display.set_mode(size)
 
 def draw():
-    rect_color = pygame.Color('red')
+    rect_color = pygame.Color('brown')
     rect_width = 0
-    rect_point = [[10,10], [280, 280]]
+    rect_point = [[50, 20], [5, 250]]
     pygame.draw.rect(screen, rect_color, rect_point, rect_width)
 
 draw()
